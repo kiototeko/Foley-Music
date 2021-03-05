65d09640d9e8a1baa4328bf8ba0aeb50aec39d6d
kiototeko <kiototeko@gmail.com>
2021-03-05 16:07:22 -0600

    cuda enabled

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ class Engine(BaseEngine):
             ignore_index=self.ds.PAD_IDX
         )
         
-        self.model: nn.Module = self.model_builder.build(device=torch.device('cpu'), wrapper=nn.DataParallel)
+        self.model: nn.Module = self.model_builder.build(device=torch.device('cuda'), wrapper=nn.DataParallel)
         
                 
         optimizer = optim.Adam(self.model.parameters(), lr=0., betas=(0.9, 0.98), eps=1e-9)
@@ -92,13 +92,13 @@ class Engine(BaseEngine):
             else:
                 raise Exception('No feature!')
 
-            """
+            
             feat, midi_x, midi_y = (
                 feat.cuda(non_blocking=True),
                 midi_x.cuda(non_blocking=True),
                 midi_y.cuda(non_blocking=True)
             )
-            """
+            
             
             if self.ds.use_control:
                 control = data['control']
